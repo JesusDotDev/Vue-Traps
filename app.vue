e75adfdf267adb6d45fc0a1f459e1ff8e5b6ca09
JesusDotDev <139908031+JesusDotDev@users.noreply.github.com>
2023-07-29 12:09:17 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,1 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Vue Todo List</title>
+  <!-- Load Vue.js from a CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+</head>
+<body>
+  <div id="app">
+    <h1>Todo List</h1>
+    
+    <!-- Input field to add new tasks -->
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Add a new task">
+    <button @click="addTask">Add</button>
+    
+    <!-- Display the list of tasks -->
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+        <button @click="removeTask(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+
+  <script>
+    new Vue({
+      el: '#app',
+      data: {
+        newTask: '', // Input field value for new task
+        tasks: [],   // Array to store tasks
+      },
+      methods: {
+        // Function to add a new task
+        addTask() {
+          if (this.newTask.trim() !== '') {
+            this.tasks.push(this.newTask);
+            this.newTask = '';
+          }
+        },
+        // Function to remove a task by index
+        removeTask(index) {
+          this.tasks.splice(index, 1);
+        },
+      },
+    });
+  </script>
+</body>
+</html>
 
